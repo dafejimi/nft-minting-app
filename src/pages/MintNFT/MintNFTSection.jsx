5e6459ce8fd906cea26ccd291a54b56f25065ae5
dafejimi <33166811+dafejimi@users.noreply.github.com>
2025-02-16 05:19:46 -0800

    Update MintNFTSection.jsx

--- src/pages/MintNFT/MintNFTSection.jsx
+++ src/pages/MintNFT/MintNFTSection.jsx
@@ -7,7 +7,7 @@ import MintSuccessSection from "./MintSuccessSection";
 /* global BigInt */  // Add this line to tell ESLint that BigInt is a global object
 
 const CONTRACT_ADDRESS = '0x743f49311a82fe72eb474c44e78da2a6e0ae951c';
-const API_BASE_URL = 'accurate-sawfish-dafe-50dc2809.koyeb.app//api/nft'; // e.g., http://localhost:3001/api/nft
+const API_BASE_URL = 'social-deana-dafe-6dd7000c.koyeb.app/api/nft'; // e.g., http://localhost:3001/api/nft
 
 export default function MintNFTSection() {
   const { address } = useAccount();
